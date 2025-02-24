abeecbf8157da8994a880c14af72ba1d242f3ffa
victorjakob <viggijakob@gmail.com>
2025-02-24 22:24:55 +0000

    topbar fix?

--- src/app/components/Topbar.jsx
+++ src/app/components/Topbar.jsx
@@ -18,11 +18,11 @@ export default function Topbar() {
   }
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-opacity-0 pointer-events-none">
+    <nav className="fixed top-0 left-0 right-0 z-50 pointer-events-none">
       <div className="container mx-auto px-4 py-2">
-        <div className="flex justify-between items-start">
+        <div className="flex justify-between items-start relative">
           {/* Logo */}
-          <Link href="/" className="pointer-events-auto">
+          <Link href="/" className="relative z-50">
             <div className="relative w-32 sm:w-32 md:w-36 lg:w-40 xl:w-48 aspect-[724/787]">
               <Image
                 src="/mamaimg/mamalogo.png"
