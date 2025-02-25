f8838c94656597798c36fd48b5b02e9d36ef3516
victorjakob <viggijakob@gmail.com>
2025-02-25 01:34:14 +0000

    ooooook

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,4 +2,12 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    storage: typeof window !== "undefined" ? localStorage : undefined, // ✅ Check if browser
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
