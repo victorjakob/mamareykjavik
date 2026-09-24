55f4d6ab60cfa58e355d021d74aee6d1010db694
victorjakob <viggijakob@gmail.com>
2026-09-24 14:33:19 +0000

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -210,6 +210,13 @@ export default async function RootLayout({ children }) {
             </AuthSessionProvider>
           </ErrorBoundary>
         </StrictMode>
+        {/* Cloudflare Web Analytics: cookieless page views, read by the Victory Studio dashboard */}
+        <Script
+          id="cf-web-analytics"
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          strategy="afterInteractive"
+          data-cf-beacon='{"token": "b50e57b9c0fa4a7785c301a3eb3071cc"}'
+        />
       </body>
     </html>
   );
